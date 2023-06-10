380d16b9f7a71df57e049a2e2869aab2c8556422
Miha Gyergyek <mihagyergyek@gmail.com>
2023-06-10 16:23:38 +0200

    Shranjevanje igre

--- Go/src/vodja/Vodja.java
+++ Go/src/vodja/Vodja.java
@@ -1,5 +1,11 @@
 package vodja;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.EnumMap;
 import java.util.Map;
 import javax.swing.SwingWorker;
 
@@ -7,6 +13,7 @@ import gui.Okno;
 import inteligenca.Inteligenca;
 import logika.Igra;
 import logika.Igralec;
+import logika.Polje;
 import splosno.Poteza;
 
 public class Vodja {
@@ -24,7 +31,83 @@ public class Vodja {
 		igramo ();
 	}
 	
+	public static void shrani(String izhod) {
+		try {
+			PrintWriter dat = new PrintWriter(new FileWriter(izhod));
+			dat.println(Igra.N);
+			dat.println(Vodja.vrstaIgralca.get(Igralec.CRNI));
+			dat.println(Vodja.vrstaIgralca.get(Igralec.BELI));
+			dat.println(igra.naPotezi());
+			dat.println(igra.skips());
+			for (int i = 0; i < Igra.N; i++) {
+				for (int j = 0; j < Igra.N; j++) {
+					dat.println(igra.getPlosca()[i][j]);
+				}
+			}
+			dat.close();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void odpriIgro(String vhod) {
+		try {
+			BufferedReader dat = new BufferedReader(new FileReader(vhod));
+			Igra.N = Integer.parseInt(dat.readLine().strip());
+			igra = new Igra();
+			Vodja.vrstaIgralca = new EnumMap<Igralec,VrstaIgralca>(Igralec.class);
+			switch (dat.readLine().strip()) {
+			case "človek":
+				Vodja.vrstaIgralca.put(Igralec.CRNI, VrstaIgralca.C);
+				break;
+			case "računalnik":
+				Vodja.vrstaIgralca.put(Igralec.CRNI, VrstaIgralca.R);
+				break;
+			}
+			switch (dat.readLine().strip()) {
+			case "človek":
+				Vodja.vrstaIgralca.put(Igralec.BELI, VrstaIgralca.C);
+				break;
+			case "računalnik":
+				Vodja.vrstaIgralca.put(Igralec.BELI, VrstaIgralca.R);
+				break;
+			}
+			switch (dat.readLine().strip()) {
+			case "Crni":
+				Vodja.igra.setNaPotezi("CRNI");
+				break;
+			case "Beli":
+				Vodja.igra.setNaPotezi("BELI");
+				break;
+			}
+			igra.setSkips(Integer.parseInt(dat.readLine().strip()));
+			for (int i = 0; i < Igra.N; i++) {
+				for (int j = 0; j < Igra.N; j++) {
+					switch (dat.readLine().strip()) {
+					case "PRAZNO":
+						Vodja.igra.getPlosca()[i][j] = Polje.PRAZNO;
+						break;
+					case "CRNO":
+						Vodja.igra.getPlosca()[i][j] = Polje.CRNO;
+						break;
+					case "BELO":
+						Vodja.igra.getPlosca()[i][j] = Polje.BELO;
+						break;
+					}
+				}
+			}
+			dat.close();
+			Vodja.igra = new Igra(igra);
+			igramo();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public static void igramo () {
+		shrani("trenutna_igra.txt");
 		okno.osveziGUI();
 		switch (igra.stanjeGo()) {
 		case ZMAGA_CRNI: 
